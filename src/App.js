fe6851e2c41b4fead7edbbd4de08ed8fbf308811
Nick Dambrosio <ndambro303@gmail.com>
2018-11-03 21:52:37 -0600

    add routes to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import LandingLoading from "./Components/LandingLoading";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingLoading />
+        {/* <LandingLoading /> */}
+        <Route exact path="/" component={LandingLoading} />
+        {/* <Route exact path="/tech" component = {techCrunch} /> */}
       </div>
     );
   }
