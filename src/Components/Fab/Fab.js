19d4ff2def2f2e5cacd84a9d1b8a3cf45ee6f6b7
Nick Dambrosio <ndambro303@gmail.com>
2018-11-04 13:39:04 -0700

    add button animation on click

--- src/Components/Fab/Fab.js
+++ src/Components/Fab/Fab.js
@@ -7,8 +7,7 @@ export default class Menu extends Component {
   constructor(props, favorites) {
     super(props);
     this.state = {
-      buttonState: "",
-      favorites: []
+      buttonState: ""
     };
   }
 
@@ -20,18 +19,19 @@ export default class Menu extends Component {
 
   render() {
     return (
-      <div className="fab-box">
-        <div className="fab-literal" onClick={this.toggleActive}>
-          <ul className={this.state.buttonState}>
-            <div>
-              <NavLink to="/home" onClick={this.props.handlePeopleLink} />
-              <NavLink to="/notes" onClick={this.props.handleNotesLink} />
-              <NavLink to="/settings" onClick={this.props.handleSettingsLink} />
-              <li />
-              <li />
-            </div>
-          </ul>
-        </div>
+      <div className="menu">
+        <div className="menu-button" onClick={this.toggleActive} />
+        <ul className={this.state.buttonState}>
+          <div className="cross">
+            <div className="cross-horizontal" />
+            <div className="cross-vertical" />
+          </div>
+          <div>
+            <NavLink to="/home" onClick={this.props.handlePeopleLink} />
+            <NavLink to="/notes" onClick={this.props.handleNotesLink} />
+            <NavLink to="/settings" onClick={this.props.handleSettingsLink} />
+          </div>
+        </ul>
       </div>
     );
   }
