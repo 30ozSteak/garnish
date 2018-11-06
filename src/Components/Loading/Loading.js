4a7e39cb9eafa52e0828491e36b3e231ef3ffeb0
Nick Dambrosio <ndambro303@gmail.com>
2018-11-05 21:54:57 -0700

    add loading animation, donezo

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -6,7 +6,15 @@ const Loading = () => {
     <div className="loading-box">
       <div className="squared" />
       <div className="loading-wrapper">
-        <p>loading</p>
+        <p>
+          <span className="loader letter1">L</span>
+          <span className="loader letter2">o</span>
+          <span className="loader letter3">a</span>
+          <span className="loader letter4">d</span>
+          <span className="loader letter5">i</span>
+          <span className="loader letter6">n</span>
+          <span className="loader letter7">g</span>
+        </p>
       </div>
     </div>
   );
