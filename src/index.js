e89d45bf46c985240832fe3bb751cff968a9a93f
Nick Dambrosio <ndambro303@gmail.com>
2018-11-06 14:05:03 -0700

    changes to setup files in index dot jay s

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { render } from "react-dom";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "../node_modules/redux-thunk";
+// import reducer from './reducers';
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
