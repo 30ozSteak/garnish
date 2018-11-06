984998baa73b5a1d4ec8d980fc56c28ff1a10ede
Nick Dambrosio <ndambro303@gmail.com>
2018-11-05 21:43:02 -0700

    delete another animation

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -6,15 +6,7 @@ const Loading = () => {
     <div className="loading-box">
       <div className="squared" />
       <div className="loading-wrapper">
-        <p>
-          <span>L</span>
-          <span>o</span>
-          <span>a</span>
-          <span>d</span>
-          <span>i</span>
-          <span>n</span>
-          <span>g</span>
-        </p>
+        <p>loading</p>
       </div>
     </div>
   );
