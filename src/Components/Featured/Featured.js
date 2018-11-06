2de0d61903b0abb3d53b68819429ecb764c42076
Nick Dambrosio <ndambro303@gmail.com>
2018-11-06 16:44:38 -0700

    clean up channel cards

--- src/Components/Featured/Featured.js
+++ src/Components/Featured/Featured.js
@@ -5,17 +5,17 @@ import Channels from "../../Containers/Channels/Channels";
 const Featured = () => (
   <div className="featured-container">
     <div className="featured-box">
-      <Channels channelName="tech" channelString="whats new in tech" />
+      <Channels
+        channelName="tech"
+        channelString="wait are hooks replacing redux?"
+      />
       <Channels
         channelName="memes"
         channelString="what the h*ck is a fortnite floss"
       />
       <Channels channelName="abramov" channelString="whatever dan says" />
       <Channels channelName="culture" channelString="learn some things" />
-      <Channels
-        channelName="top"
-        channelString="for when you want to be depressed"
-      />
+      <Channels channelName="top" channelString="the depressing stuff" />
     </div>
     <p className="scroll-text">scroll for more</p>
   </div>
