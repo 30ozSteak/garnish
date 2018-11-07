83838e1b7d4f514c3cd5b38c50afe287cf6e1bac
Nick Dambrosio <ndambro303@gmail.com>
2018-11-07 13:58:10 -0700

    clean up indexdotjayess

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import { darkMode } from "./darkModeReducer";
-import { isLoading } from "./isLoadingReducer";
 import { initialLoading } from "./initialLoadingReducer";
 import { fabActive } from "./fabActive";
 
