a1dbf0dc492ba825b3257fcdcb73ebe0719ea93b
Nick Dambrosio <ndambro303@gmail.com>
2018-11-07 08:01:24 -0700

    remove news container

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./News.css";
 
-const News = ({ title, urlToImage, description, url }) => {
-  return (
-    <div className="news-box">
-      <h1> Your news </h1>
-      <div className="cat-box">
-        <h1 className="article-title">{title}</h1>
-        <img src={urlToImage} alt="" />
-        <p className="article-desc">{description}</p>
-        <a href={url}> read more..</a>
+const News = ({ news, title, urlToImage, description, url }) => {
+  let newsArray = news;
+
+  const newsCard = newsArray.map(article => {
+    return (
+      <div className="news-box">
+        <h1> Your news </h1>
+        <div className="cat-box">
+          <h1 className="article-title">{article.title}</h1>
+          <img src={article.urlToImage} alt="" />
+          <p className="article-desc">{article.description}</p>
+          <a href={article.url}> read more..</a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  });
 };
 
 export default News;
