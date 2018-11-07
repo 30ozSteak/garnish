d91045165397ca4d62107240e32c762238d1f587
Nick Dambrosio <ndambro303@gmail.com>
2018-11-06 22:00:45 -0700

    bug squashing index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { render } from "react-dom";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 import thunk from "../node_modules/redux-thunk";
-import reducer from "./Reducers/index";
+
+import { rootReducer } from "./Reducers/index";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
