3ce8cac1129fb33cafe970446cc5e8dc4d74a283
Nick Dambrosio <ndambro303@gmail.com>
2018-11-06 21:50:56 -0700

    add proper toggle darkmode reducer

--- src/Reducers/darkModeReducer.js
+++ src/Reducers/darkModeReducer.js
@@ -1,3 +1,3 @@
 export const darkMode = (state = false, action) => {
-  return action.type === "TOGGLE_DARK" ? action.status : state;
+  return action.type === "TOGGLE_DARKMODE" ? action.status : state;
 };
