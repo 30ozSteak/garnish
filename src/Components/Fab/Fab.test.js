3f9289f707ffba8c7ddca476324bcc21a0bfcaaf
Nick Dambrosio <ndambro303@gmail.com>
2018-11-07 20:31:52 -0700

    add fab tests

--- src/Components/Fab/Fab.test.js
+++ src/Components/Fab/Fab.test.js
@@ -7,4 +7,12 @@ describe("Fab", () => {
     const wrapper = shallow(<Fab />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it("should change state on click", async () => {
+    const expected = "active";
+    const wrapper = shallow(<Fab />);
+    wrapper.setState({ buttonState: "" });
+    await wrapper.instance().toggleActive();
+    expect(wrapper.state("buttonState")).toEqual(expected);
+  });
 });
