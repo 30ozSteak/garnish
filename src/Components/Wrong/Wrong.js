4ab6fea821098d355ec0eb3e923f889090b167af
Nick Dambrosio <ndambro303@gmail.com>
2018-11-07 21:10:15 -0700

    clean up wrong way box

--- src/Components/Wrong/Wrong.js
+++ src/Components/Wrong/Wrong.js
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 const Wrong = () => {
   return (
     <div className="wrong-way-box">
-      <h1> 404 </h1>
+      <h1> y̷̨̦̪̎̽̀͋ō̸̱̰̤͎̈́̎͘͝u̸͉͚̥͎̬͑̋ ̸̨̛̲͉͔͗̿̔͑s̶̹̻̎̒̈̔h̴̲̮̻̱͓͝o̵͙̰̥̬̒̈́̓ū̵̥͉͚͎͍͌l̴̗̗͉̱̄̌ḋ̴̛̫͉̪̦͋n̷̬͔͂͛'̶̢̞͎́̀͜t̵̪̘̀̔ ̵͖͙̿́͌͗b̸̧͎̿̀̌͊̾ę̵͙͍̏̍̑̕ ̵͚̝̥͍̆͛̕͜h̶͙̭͙͙͍̅̇̔̽͠e̴̠̾̂̓̿͘r̸̝̳̓e̴͎͐̅̓̈̄ </h1>
       <NavLink to="/main">
         <h1>Go Back</h1>
       </NavLink>
